3fab1b72bff3c4b48c91c73334da0d15c32b136c
Francisco Orvalho <francisco.orvalho@surfdome.com>
2014-01-28 18:48:45 +0000

    Allow setting key and base_url for Api. Add request methods.

--- lib/league_of_legends/api.rb
+++ lib/league_of_legends/api.rb
@@ -1,12 +1,66 @@
+require 'league_of_legends/request/all'
+
 module ::LeagueOfLegends
   class Api
 
-    def self.key
-      'a1a1a1-b2b2b2-c3c3c3-d4d4d4-e5e5e5e5'
+    attr_reader :key, :base_url
+
+    def initialize options = {}
+      case options
+      when Hash
+        opt = self.class.default_options.merge(options)
+        set_key(opt[:key])
+        set_base_url(opt[:base_url])
+      when String
+        set_key(options)
+        set_base_url(self.class.default_options[:base_url])
+      else
+        raise ArgumentError.new 'expected a Hash or a String'
+      end
     end
 
-    def self.base_url
-      'https://prod.api.pvp.net/api/lol'
+    def self.default_options
+      {
+        key: nil,
+        base_url: 'https://prod.api.pvp.net/api/lol'
+      }
+    end
+
+    def get request, *arguments
+      req_class = ::LeagueOfLegends::Request::Mapper.get(request.to_sym)
+      request = req_class.new self, *arguments
+      request.response
+    end
+
+    def available_requests
+      ::LeagueOfLegends::Request::Mapper.available
+    end
+
+    private
+
+    def set_key key = nil
+      case key
+      when nil
+        raise ArgumentError.new 'wrong number of arguments (0 for 1)'
+      when String
+        if key =~ /^\h{8}(-\h{4}){3}-\h{12}$/
+          @key = key
+        else
+          raise ArgumentError.new 'key with incorrect format'
+        end
+      else
+        raise ArgumentError.new 'expected a String'
+      end
+    end
+
+    def set_base_url base_url
+      require 'uri'
+
+      if base_url =~ /^#{URI::regexp}$/o
+        @base_url = base_url
+      else
+        raise ArgumentError.new 'expected a valid URL'
+      end
     end
 
   end
--- spec/league_of_legends/api_spec.rb
+++ spec/league_of_legends/api_spec.rb
@@ -2,13 +2,25 @@ require 'league_of_legends/api'
 
 describe ::LeagueOfLegends::Api do
 
-  it "has an API key" do
-    expect(described_class.key).to be_an_instance_of String
-    expect(described_class.key.length).to eq 36
+  let(:api) { ::LeagueOfLegends::Api.new '0c78469b-b773-4b35-9f6a-00d7fe964290
+' }
+
+  it "has access to an API key" do
+    pending "testing with real key"
+    expect(api.key).to eq 'a1a1a1a1-a1a1-a1a1-a1a1-a1a1a1a1a1a1'
   end
 
   it "has a base url" do
-    expect(described_class.base_url).to eq 'https://prod.api.pvp.net/api/lol'
+    expect(api.base_url).to eq 'https://prod.api.pvp.net/api/lol'
+  end
+
+  it "can get a list of available requests" do
+    expect(api.available_requests).to be_kind_of Enumerable
+  end
+
+  it "can send requests and receive a response" do
+    pending "requires internet connection"
+    expect(api.get(:summoner_stats_summary, 12345)).to be_an_instance_of ::LeagueOfLegends::DTO::PlayerStatsSummaryList
   end
 
 end
