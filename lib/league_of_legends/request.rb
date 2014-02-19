7e808f53b7e57b05b6c585a86e744b9f7be52f5d
Francisco Orvalho <francisco.orvalho@surfdome.com>
2014-02-19 09:22:29 +0000

    Get rid of useless request/all

--- lib/league_of_legends/request.rb
+++ lib/league_of_legends/request.rb
@@ -1,4 +1,3 @@
-require 'league_of_legends/request/all'
 require 'league_of_legends/request/base'
 require 'league_of_legends/request/champion'
 require 'league_of_legends/request/game/by_summoner/recent'
