67712c3a3d32d36ee913c3a1f958038050ffe286
Francisco Orvalho <francisco.orvalho@surfdome.com>
2015-10-28 11:45:27 +0000

    Update Team DTO to v2.4

--- lib/league_of_legends/dto/team.rb
+++ lib/league_of_legends/dto/team.rb
@@ -1,16 +1,14 @@
 module ::LeagueOfLegends
   module DTO
     class Team < ::LeagueOfLegends::DTO::Base
-
       def self.version
-        'v2.2'
+        'v2.4'
       end
 
       attr_reader :create_date, :full_id, :last_game_date, :last_join_date,
                   :last_joined_ranked_team_queue_date, :match_history,
-                  :message_of_day, :modify_date, :name, :roster,
-                  :second_last_join_date, :status, :tag, :team_stat_summary,
-                  :third_last_join_date
+                  :modify_date, :name, :roster, :second_last_join_date, :status,
+                  :tag, :team_stat_details, :third_last_join_date
 
       def initialize(attributes)
         return if attributes.nil?
@@ -24,19 +22,17 @@ module ::LeagueOfLegends
         @match_history = attributes[:matchHistory].map do |match|
           ::LeagueOfLegends::DTO::MatchHistorySummary.new(match)
         end
-        @message_of_day = ::LeagueOfLegends::DTO::MessageOfDay
-          .new(attributes[:messageOfDay])
         @modify_date = Time.at(attributes[:modifyDate] / 1000)
         @name = attributes[:name]
         @roster = ::LeagueOfLegends::DTO::Roster.new(attributes[:roster])
         @second_last_join_date = Time.at(attributes[:secondLastJoinDate] / 1000)
         @status = attributes[:status]
         @tag = attributes[:tag]
-        @team_stat_summary = ::LeagueOfLegends::DTO::TeamStatSummary
-          .new(attributes[:teamStatSummary])
+        @team_stat_details = attributes[:teamStatDetails].map do |detail|
+          ::LeagueOfLegends::DTO::TeamStatDetail.new(detail)
+        end
         @third_last_join_date = Time.at(attributes[:thirdLastJoinDate] / 1000)
       end
-
     end
   end
 end
